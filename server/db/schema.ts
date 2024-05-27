33a8f4e9184a836e11e7291e82ad9723a11927a6
Samuel Lembke <samuel.lembke@stratonx.com>
2024-05-27 13:42:18 +0200

    Init

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,7 +1,8 @@
 import { relations, sql } from "drizzle-orm";
 import {
+  boolean,
   index,
-  integer,
+  integer, jsonb,
   pgTableCreator,
   primaryKey,
   serial,
@@ -12,6 +13,106 @@ import {
 import { type AdapterAccount } from "next-auth/adapters";
 
 export const createTable = pgTableCreator((name) => `llm-ui_${name}`);
+
+export const inferenceProvider = createTable("inferenceProvider", {
+  id: serial("id").primaryKey(),
+  providerName: varchar("providerName", { length: 255 }).notNull(),
+  accountName: varchar("accountName", { length: 255 }).notNull(),
+  userId: varchar("userId", { length: 255 }).notNull().references(() => users.id),
+})
+
+export const inferenceProviderRelations = relations(inferenceProvider, ({ one, many }) => ({
+  user: one(users, { fields: [inferenceProvider.userId], references: [users.id] }),
+  inferenceProviderCredentials: many(inferenceProviderCredentials),
+  inferenceSource: many(inferenceSource),
+}));
+
+export const inferenceProviderCredentials = createTable("inferenceProviderCredentials", {
+  id: serial("id").primaryKey(),
+  inferenceProviderId: integer("inferenceProviderId").notNull().references(() => inferenceProvider.id),
+  credentialKey: varchar("credentialKey", { length: 255 }).notNull(),
+  credentialValue: text("credentialValue").notNull(),
+})
+
+export const inferenceProviderCredentialsRelations = relations(inferenceProviderCredentials, ({ one }) => ({
+  inferenceProvider: one(inferenceProvider, { fields: [inferenceProviderCredentials.inferenceProviderId], references: [inferenceProvider.id] }),
+}));
+
+export const inferenceSource = createTable("inferenceSource", {
+  id: serial("id").primaryKey(),
+  providerId: integer("providerId").notNull().references(() => inferenceProvider.id),
+  type: varchar("type", { length: 255 }).notNull(),
+  name: varchar("name", { length: 255 }).notNull(),
+})
+
+export const inferenceSourceRelations = relations(inferenceSource, ({ one, many }) => ({
+  inferenceProvider: one(inferenceProvider, { fields: [inferenceSource.providerId], references: [inferenceProvider.id] }),
+  inferenceMessages: many(inferenceMessage),
+}));
+
+export const thread = createTable("threads", {
+  id: serial("id").primaryKey(),
+  ownerId: varchar("ownerId", { length: 255 }).notNull().references(() => users.id),
+  name: varchar("name", { length: 255 }).notNull(),
+  createdAt: timestamp("createdAt", {
+    mode: "date",
+    withTimezone: true,
+  }).default(sql`CURRENT_TIMESTAMP`),
+})
+
+export const threadRelations = relations(thread, ({ one, many }) => ({
+  owner: one(users, { fields: [thread.ownerId], references: [users.id] }),
+  inferenceMessages: many(inferenceMessage),
+  userMessages: many(userMessages),
+}));
+
+export const inferenceMessage = createTable("inferenceMessage", {
+  id: serial("id").primaryKey(),
+  createdAt: timestamp("createdAt", {
+    mode: "date",
+    withTimezone: true,
+  }).default(sql`CURRENT_TIMESTAMP`),
+  sourceId: integer("sourceId").notNull().references(() => inferenceSource.id),
+  threadId: integer("threadId").notNull().references(() => thread.id),
+  type: varchar("type", { length: 255 }).notNull(),
+  finishedStreaming: timestamp("finishedStreaming", {
+    mode: "date",
+    withTimezone: true,
+  }).default(sql`NULL`),
+  content: jsonb("content").$type<{
+    message: string;
+    jupyterData: null | {
+
+    }
+    imageId: string;
+  }>().notNull(),
+})
+
+export const inferenceMessageRelations = relations(inferenceMessage, ({ one }) => ({
+  inferenceSource: one(inferenceSource, { fields: [inferenceMessage.sourceId], references: [inferenceSource.id] }),
+  thread: one(thread, { fields: [inferenceMessage.threadId], references: [thread.id] }),
+}));
+
+export const userMessages = createTable("userMessages", {
+  id: serial("id").primaryKey(),
+  createdAt: timestamp("createdAt", {
+    mode: "date",
+    withTimezone: true,
+  }).default(sql`CURRENT_TIMESTAMP`),
+  userId: varchar("userId", { length: 255 }).notNull().references(() => users.id),
+  threadId: integer("threadId").notNull().references(() => thread.id),
+  type: varchar("type", { length: 255 }).notNull(),
+  content: jsonb("content").$type<{
+    message: string;
+    imageId: string | null;
+    documentId: string | null;
+  }>().notNull(),
+})
+
+export const userMessagesRelations = relations(userMessages, ({ one }) => ({
+  user: one(users, { fields: [userMessages.userId], references: [users.id] }),
+  thread: one(thread, { fields: [userMessages.threadId], references: [thread.id] }),
+}));
 
 export const users = createTable("user", {
   id: varchar("id", { length: 255 }).notNull().primaryKey(),
@@ -26,6 +127,9 @@ export const users = createTable("user", {
 
 export const usersRelations = relations(users, ({ many }) => ({
   accounts: many(accounts),
+  inferenceProviders: many(inferenceProvider),
+  threads: many(thread),
+  userMessages: many(userMessages),
 }));
 
 export const accounts = createTable(
