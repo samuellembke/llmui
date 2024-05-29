51e17551bd74ad0ddbcbd8a3beb31bd50d3be5ce
Samuel Lembke <samuel.lembke@stratonx.com>
2024-05-29 12:11:31 +0200

    Implementing chatting and ai

--- components/dashboard/chats/Chat.tsx
+++ components/dashboard/chats/Chat.tsx
@@ -7,6 +7,8 @@ import {useMessages} from "@/components/dashboard/chats/MessageProvider";
 import {api} from "@/trpc/react";
 import {useChat} from "ai/react";
 import Markdown from "react-markdown";
+import {Textarea} from "@/components/ui/textarea";
+import {cn} from "@/lib/utils";
 
 type userThread = typeof thread.$inferSelect
 type userMessage = typeof userMessages.$inferSelect
@@ -25,7 +27,7 @@ type iMessage = {
 function UserMessage({content}: { content: string }) {
   return (
     <div className="flex flex-row justify-end gap-2">
-      <div className="bg-white/10 p-2 rounded-md text-end text-[#bbb7cd] markdown">
+      <div className="bg-white/10 p-2 rounded-md text-end  ">
         <Markdown>
           {content}
         </Markdown>
@@ -37,7 +39,7 @@ function UserMessage({content}: { content: string }) {
 function InferenceMessage({content}: { content: string }) {
   return (
     <div className="flex flex-row justify-start gap-2">
-      <div className="bg-white/10 p-2 rounded-md text-start text-[#bbb7cd] markdown">
+      <div className=" rounded-md text-start prose prose-invert">
         <Markdown>
           {content}
         </Markdown>
@@ -48,9 +50,14 @@ function InferenceMessage({content}: { content: string }) {
 
 
 export default function Chat() {
-
+  const [streaming, setStreaming] = React.useState(false)
+  const [autoScroll, setAutoScroll] = React.useState(true)
   const scrollRef = React.useRef<HTMLDivElement>(null)
+  const scrollWrapperRef = React.useRef<HTMLDivElement>(null)
+  const endScrollRef = React.useRef<HTMLDivElement>(null)
+  const inputRef = React.useRef<HTMLTextAreaElement>(null)
   const { activeThread } = useChats()
+  const prevThreadID = React.useRef<number | null>(null)
   // const { userMessages, inferenceMessages, setActiveThread } = useMessages()
   const userMessagesQuery = api.messages.getUserMessages.useQuery({
     threadId: activeThread?.id ?? 0,
@@ -110,8 +117,16 @@ export default function Chat() {
         id: "empty-"+Math.random(),
       }
     }),
+    onResponse: (response) => {
+      setStreaming(true)
+      if (inputRef.current) {
+        inputRef.current.value = ""
+      }
+    },
     onFinish: (message) => {
       if (message.role === "assistant") {
+        setStreaming(false)
+        inputRef.current?.focus()
         console.log(`Saving message for thread ${activeThread!.id}`)
         sendInferenceMessage.mutate({
           threadId: activeThread!.id,
@@ -134,20 +149,61 @@ export default function Chat() {
   })
 
   useEffect(() => {
-    if (scrollRef.current) {
-      scrollRef.current.scrollTop = scrollRef.current.scrollHeight
+    const handleResize = () => {
+      if (autoScroll) {
+        endScrollRef.current?.scrollIntoView({ behavior: "instant" })
+      }
     }
-  }, [messages])
+    const scroll = scrollWrapperRef.current
+
+    const observer = new ResizeObserver(handleResize)
+    if (scroll) {
+      observer.observe(scroll)
+    }
+
+
+    return () => {
+      if (scroll) {
+        observer.unobserve(scroll)
+      }
+      observer.disconnect()
+    }
+
+  }, [autoScroll])
+
+  const handleScroll = () => {
+    const chat = scrollRef.current
+
+    if (chat) {
+      const isAtBottom = Math.abs((chat.scrollHeight - chat.scrollTop) - chat.clientHeight)
+      setAutoScroll(isAtBottom < 80)
+    }
+  }
+
+  useEffect(() => {
+    if (!streaming) {
+      inputRef.current?.focus()
+      if (inputRef.current) {
+        inputRef.current.value = ""
+      }
+    }
+  }, [streaming])
+
+  useEffect(() => {
+    if (prevThreadID.current !== activeThread?.id) {
+      prevThreadID.current = activeThread?.id ?? null
+    }
+  }, [activeThread]);
 
 
   return (
     <div className="h-full max-h-full w-full bg-background flex flex-col justify-start items-start">
       <div className="w-full h-full flex-grow flex flex-row justify-center items-stretch">
-        <div className="w-full h-full relative max-w-[48rem]">
+        <div className="w-full h-full relative">
           <div
-            className="absolute top-0 start-0 w-full h-full flex overflow-auto" ref={scrollRef}>
+            className="absolute top-0 start-0 w-full h-full flex flex-row justify-center items-stretch overflow-auto" ref={scrollRef} onScroll={handleScroll}>
             <div
-              className="w-full h-fit flex flex-col justify-start items-stretch gap-[2rem] p-[1rem]">
+              className="w-full h-fit flex flex-col justify-start items-stretch gap-[2rem] p-[1rem] max-w-[48rem]" ref={scrollWrapperRef}>
               {aiMessages.map((message, index) => {
                 return (
                   <Fragment key={index}>
@@ -160,11 +216,12 @@ export default function Chat() {
                   </Fragment>
                 )
               })}
+              <div className="h-0" ref={endScrollRef}></div>
             </div>
           </div>
         </div>
       </div>
-      <div className="w-full px-[1rem] py-[2rem]">
+      <div className={cn(`w-full px-[1rem] py-[2rem]`, `${activeThread?.id != null ? '' : 'hidden'}`)}>
         <form onSubmit={(e) => {
           sendUserMessage.mutate({
             threadId: activeThread!.id,
@@ -175,7 +232,14 @@ export default function Chat() {
           })
           handleSubmit(e)
         }}>
-          <Input placeholder="Type a message" className="w-full" value={input} onChange={handleInputChange}/>
+          <Textarea ref={inputRef} disabled={streaming} placeholder="Type a message" className="w-full resize-none" value={input} onChange={handleInputChange} onKeyDown={(e) => {
+            if (e.key === "Enter" && !e.shiftKey) {
+              console.log("Submitting", e)
+              // @ts-ignore
+              e.target.parentElement?.requestSubmit()
+              setStreaming(true)
+            }
+          }}/>
         </form>
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,7 +78,10 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require("tailwindcss-animate")
+  ],
 } satisfies Config
 
 export default config
